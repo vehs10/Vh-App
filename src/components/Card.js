2aef77c703a0ad891af39d0bfe4bf64af662b53c
vehs10 <vehs03@gmail.com>
2021-06-09 14:46:34 -0300

    ContadorCard

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import {Button} from 'react-bootstrap'
-
+import React from "react";
+import { Button } from "react-bootstrap";
 
 function Card(props) {
-    return(
-        <div className="card col-lg-3 col-sm-10 m-3">
-            <img src="" alt="img-Card" />
-            <div className="card-body">
-                <h4 className="card-title">{props.name}</h4>
-                <p className="card-text text-center">Type: {props.type}</p>
-                <Button variant="primary">Obtener</Button>
-            </div>
-            
-        </div>
-    )
+  function aumentarContador(event) {
+    setClicks(clicks + 1);
+  }
+
+  function restarContador(event) {
+    setClicks(clicks - 1);
+  }
+
+  const [clicks, setClicks] = React.useState(0);
+
+  return (
+    <div className="card col-lg-3 col-sm-10 m-3">
+      <img src="" alt="img-Card" />
+      <div className="card-body">
+        <h4 className="card-title">{props.name}</h4>
+        <p className="card-text text-center">Type: {props.type}</p>
+        <Button className="mr-2" variant="success" onClick={aumentarContador}>+</Button>
+        <Button variant="danger" onClick={restarContador}>-</Button>
+        <p>{`Cantidad:${clicks}`}</p>
+        <Button variant="primary">Obtener</Button>
+      </div>
+    </div>
+  );
 }
-export default Card
+export default Card;
