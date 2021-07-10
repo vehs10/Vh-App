315796641d148893f5f2eb3eb33c1bfabb449b2a
vehs10 <vehs03@gmail.com>
2021-07-10 02:21:17 -0300

    Mostrando datos de la firebase en About.js

--- src/firebase/ContainerFirebase.js
+++ src/firebase/ContainerFirebase.js
@@ -3,22 +3,49 @@ import React, { useEffect, useState } from "react";
 import { getFirestore } from './Firebase'
 
 function ContainerFirebase() {
-    const [item, Setitem] = useState([]);
-    useEffect(() =>{
-        const db = getFirestore();
-        db.get()
-        .then((response) => response.json())
-        .then((response) =>{
-          Setitem(response)
-          console.log(response);
-          console.log(item);
-        })
-      
-      }, [])
-    
+  const [item, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    const db = getFirestore();
+    const itemCollection = db.collection("item");
+    itemCollection
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.size === 0) {
+          console.log(" no hay resultados");
+        }
+        setItems(querySnapshot.docs.map((doc) => doc.data()));
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
     return (
-        <div></div>
+        <div className="Card-style container p-4">
+        <h3 className="text-center font-weight-bold p-4">Información Sobre Nosotros</h3>
+        {item.length > 0 ? (
+        item.map((data, index) => {
+          return (
+            <ul className="lista text-left">
+              <li className="m-4"><span className="font-weight-bold">Nombre del Desarrollador:</span> {data.name}</li>
+              <li className="m-4"><span className="font-weight-bold">Edad:</span> {data.age}</li>
+              <li className="m-4"><span className="font-weight-bold">Empresa:</span> {data.business}</li>
+              <li className="m-4"><span className="font-weight-bold">API utilizada para el desarrollo de la aplicación:</span> {data.Api}</li>
+              <li className="m-4"><span className="font-weight-bold">Nuestro Objetivo:</span> {data.text}</li>
+            </ul>
+          );
+        })
+      ) : (
+        <h3>Cargando...</h3>
+      )}
+        </div>
+
+
        
     )
         
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,13 @@
 import React from 'react'
- //import ContainerFirebase from '../firebase/ContainerFirebase'
+ import ContainerFirebase from '../firebase/ContainerFirebase'
 
 
 function About() {
     return (
         <div className="pt-4 mt-4 container text-center">
-            <h1>About</h1>
-            <p>Todo sobre nosotros</p>
+            <h1 className="m-4 p-3">PokeApi VH</h1>
+           
+            <ContainerFirebase />
            
         </div>
     )
